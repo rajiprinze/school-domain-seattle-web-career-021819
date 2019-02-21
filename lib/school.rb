0ccef7a824f7381c0a37d3fd5ae5a3879441ffc8
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-21 00:25:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
   def add_student(student, grade)
     if !(@roster == nil)
-      @roster = {:item1 => 1}
+      # @roster = {:item1 => 1}
       @roster[grade] = []
       @roster[grade] << student
       # @roster => {student =>[grade]}
