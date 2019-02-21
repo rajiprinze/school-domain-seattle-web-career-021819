bf4944f5a454668d4e1df76f83e39a27ecfa142a
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-21 00:02:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+# code here!
+# code here!
+class School
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster.merge(student=>grade)
+  end
+
+  def grade (this_grade)
+    roster
+  end
+
+  def sort()
+    roster
+  end
+end
